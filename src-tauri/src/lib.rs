f5deeef28476c7e2d088d69cc5d48802abae2b74
Randy Brown <thatrandybrown@gmail.com>
2025-12-26 22:52:21 -0600

    initialize a hermenia machine

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+use serde_json::{Value};
+
 mod hermenia;
 use hermenia::{Machine};
 
@@ -12,6 +17,18 @@ pub fn run() {
             .build(),
         )?;
       }
+
+      let data: HashMap<String, Value> = HashMap::new();
+      let mut listeners: Vec<Box<dyn Fn(&str, &Value) + Send + Sync>> = Vec::new();
+      let reducers: HashMap<String, (Value, fn(Value, &str, &str) -> Value)> =
+        HashMap::new();
+
+      let machine = Machine::new(
+          data,
+          reducers,
+          Mutex::new(std::mem::take(&mut listeners)),
+      );
+
       Ok(())
     })
     .run(tauri::generate_context!())
