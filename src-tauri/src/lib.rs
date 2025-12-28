8535285161ae2adb258d2e16b3996b873e4df3ec
Randy Brown <thatrandybrown@gmail.com>
2025-12-27 21:46:03 -0600

    expose dispatch

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -44,6 +44,7 @@ pub fn run() {
 
       Ok(())
     })
+    .invoke_handler(tauri::generate_handler![dispatch])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
