cb2b68031289b0b33ba8863f7f20a107d30720ee
Randy Brown <thatrandybrown@gmail.com>
2025-12-27 21:50:09 -0600

    cargo fmt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
 
+use serde_json::Value;
 use tauri::Manager;
-use serde_json::{Value};
 
 mod hermenia;
 use hermenia::{Machine};
@@ -19,32 +19,27 @@ fn dispatch(
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
 
-      let data: HashMap<String, Value> = HashMap::new();
-      let mut listeners: Vec<Box<dyn Fn(&str, &Value) + Send + Sync>> = Vec::new();
-      let reducers: HashMap<String, (Value, fn(Value, &str, &str) -> Value)> =
-        HashMap::new();
+            let data: HashMap<String, Value> = HashMap::new();
+            let mut listeners: Vec<Box<dyn Fn(&str, &Value) + Send + Sync>> = Vec::new();
+            let reducers: HashMap<String, (Value, fn(Value, &str, &str) -> Value)> = HashMap::new();
 
-      let machine = Machine::new(
-          data,
-          reducers,
-          Mutex::new(std::mem::take(&mut listeners)),
-      );
+            let machine = Machine::new(data, reducers, Mutex::new(std::mem::take(&mut listeners)));
 
-      app.manage(machine);
+            app.manage(machine);
 
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![dispatch])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![dispatch])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
