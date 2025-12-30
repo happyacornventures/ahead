563c1f2e95ee56adb4152cfbf070502a946a5437
Randy Brown <thatrandybrown@gmail.com>
2025-12-29 22:22:29 -0600

    create tamagui theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
+import { defaultConfig } from '@tamagui/config/v4'
+import { createTamagui } from 'tamagui'
+
+const tamaguiConfig = createTamagui(defaultConfig)
+
+type Conf = typeof tamaguiConfig
+
+// make imports typed
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends Conf {}
+}
+
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends Conf {}
+}
 export default function RootLayout() {
   return <Stack
     screenOptions={{
