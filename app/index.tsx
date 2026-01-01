8a77c40cc51c5891bc70dd21665436a31db24b5b
Randy Brown <thatrandybrown@gmail.com>
2025-12-31 21:09:41 -0600

    import invoke to call tauri functions

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,9 @@ import { View } from "react-native";
 
 import { Button, Text } from 'tamagui';
 
+import { invoke } from '@tauri-apps/api/core';
+import { useEffect } from "react";
+
 function Demo() {
   return <Button theme="blue">Hello world</Button>
 }
