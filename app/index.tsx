26dd1a25d170ca30935d29c8a740b73703ca0578
Randy Brown <thatrandybrown@gmail.com>
2026-01-01 19:41:29 -0600

    print nodes maybe

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,20 @@
 import { View } from "react-native";
 
-import { Button, Text } from 'tamagui';
+import { Button, ListItem, Text } from 'tamagui';
 
 import { invoke } from '@tauri-apps/api/core';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Demo() {
   return <Button theme="blue">Hello world</Button>
 }
 
 export default function Index() {
+  const [nodes, setNodes] = useState<Record<string, unknown>>({});
 
   useEffect(() => {
-    invoke("dispatch", { event: "AppStarted" }).then(rsp => JSON.parse(rsp as string)).then(console.log).catch(console.error);
-  });
+    invoke("dispatch", { event: "AppStarted" }).then(rsp => JSON.parse(rsp as string)).then(setNodes).catch(console.error);
+  }, []);
 
   return (
     <View
@@ -24,6 +25,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      {Object.values(nodes).map((node, index) => (<ListItem key={index}>{node}</ListItem>))}
       <Demo />
     </View>
   );
