58a070c8fbb2747b966317c4926befcfc19e3802
Randy Brown <thatrandybrown@gmail.com>
2026-01-02 21:23:56 -0600

    print the slug, rather than the full node

--- app/index.tsx
+++ app/index.tsx
@@ -27,7 +27,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      {Object.values(nodes).map((node, index) => (<ListItem key={index}>{node}</ListItem>))}
+      {Object.values(nodes).map((node, index) => (<ListItem key={index}>{node?.slug}</ListItem>))}
       <Button theme="blue" onPress={() => createNode("example-slug").then(rsp => JSON.parse(rsp as string)).then(console.log)}>Hello world</Button>
     </View>
   );
