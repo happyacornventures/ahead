6c9ecb2b2b007528d81169819ee8ab835365f8c4
Randy Brown <thatrandybrown@gmail.com>
2026-01-03 21:35:00 -0600

    add the modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout() {
       <Theme name="light">
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         </Stack>
       </Theme>
     </TamaguiProvider>
